68182c4cb31a5ffc9417b538e3609afe56fddc09
afreenzameer <90803406+afreenzameer@users.noreply.github.com>
2022-02-02 19:57:53 -0600

    Update Circle.java

--- src/main/java/edu/ben/lab_1/Circle.java
+++ src/main/java/edu/ben/lab_1/Circle.java
@@ -1,1 +1,33 @@
 
+package edu.ben.lab_1;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+
+    }
+
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            System.out.println("radius cannot be negative");
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    @Override
+    public Double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+
+    @Override
+    public Double area() {
+        return Math.PI * radius * radius;
+    }
+}
